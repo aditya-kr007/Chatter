562dac829a534ea44d1c8540d8fb95efd107eb00
aditya-kr007 <adityakr.mnnit@gmail.com>
2025-10-16 00:05:05 +0530

    adding App.css and App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,26 @@
+import { Routes, Route, Navigate } from 'react-router-dom'
+import './App.css'
+import Home from './pages/home/Home'
+import Login from './pages/login/Login'
+import Signup from './pages/signup/Signup'
+import { Toaster } from 'react-hot-toast'
+import { AuthContextProvider, useAuthContext } from '../context/AuthContext'
+
+function App() {
+  const {authUser}=useAuthContext()
+  return (
+    <>
+    <div className='p-4 h-screen flex items-center justify-center'>
+    <Toaster/>
+      <Routes>
+        <Route path='/' element={authUser? <Home />: <Navigate to='/login' />} />
+        <Route path='/login' element={authUser? <Navigate to='/' />:<Login />} />
+        <Route path='/signup' element={ authUser? <Navigate to={'/'}/>: <Signup /> } />
+      </Routes>
+  
+    </div>
+    </>
+  )
+}
+
+export default App
